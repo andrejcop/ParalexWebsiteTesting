e0828791b3bfef3a7bd9cd8683092307443d61d4
andrejcop <andrej7.cop@gmail.com>
2020-08-31 21:39:52 +0200

    h1 improvements

--- script/script.js
+++ script/script.js
@@ -1,11 +1,14 @@
 window.addEventListener("scroll", test);
 var paralex1 = document.getElementById("paralex1");
 var paralex3 = document.getElementById("paralex3");
+var h1 = document.getElementById("h1");
 
 function test() {
     var x = document.documentElement.scrollTop;
-    console.log("translate(0," + x +")");
+    
     paralex1.style.backgroundPositionY = -x/3 + "px";
     paralex3.style.backgroundPositionY = -x/3+300 + "px";
+    h1.style.boxShadow = "5px " + (10-x/40) + "px";
+    console.log("5px " + (10 - x/9) + "px");
 
 }
